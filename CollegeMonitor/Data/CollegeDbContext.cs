a6c43d5828d09ccf2ba92f60867aae65718c31a9
shivraj0822 <daman.gautam0822@gmail.com>
2024-04-27 21:27:31 +0545

    Configure Db Context

---
+++ CollegeMonitor/Data/CollegeDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class CollegeDbContext: DbContext
+{
+    public DbSet<Course> Courses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite ("Data Source=CollegeMonitor.db");
+    }
+}
