50140ae8539af419e8b362ee5d2a06287aabd2e6
shivraj0822 <daman.gautam0822@gmail.com>
2024-04-30 12:22:15 +0545

    Add edit,delete ,index ,details page in courses

---
+++ CollegeMonitor/Pages/Courses/Create.cshtml .cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class CreateCourseModel: PageModel
+{
+     [BindProperty]
+     public Course Course {get; set;}
+
+     public IActionResult OnPost()
+     {
+        CollegeDbContext db = new();
+        db.Courses.Add(Course);
+        db.SaveChanges();
+
+        return RedirectToPage("Courses");
+     }
+}
---
+++ CollegeMonitor/Pages/Sessions/Delete.cshtml.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollegeMonitor.Pages_Sessions
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly CollegeDbContext _context;
+
+        public DeleteModel(CollegeDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Session Session { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Session.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (session == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Session = session;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Session.FindAsync(id);
+            if (session != null)
+            {
+                Session = session;
+                _context.Session.Remove(Session);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
